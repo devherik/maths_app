5bfdd77ae794f946bcef8fd77641e0b99ba64e52
Herik Colares <dev.herik@gmail.com>
2023-08-13 19:08:39 -0300

    before change structuro from homepage

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -147,129 +147,134 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
       body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 20),
         decoration: BoxDecoration(
           image: DecorationImage(
               image: AssetImage(globals.backgroundImage), fit: BoxFit.cover),
         ),
-        child: Container(
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20), color: Colors.white60),
-          padding: const EdgeInsets.only(left: 40, right: 40),
-          margin:
-              const EdgeInsets.only(top: 200, bottom: 10, right: 10, left: 10),
-          child: Align(
-            alignment: Alignment.center,
-            child: ListView(
-              children: <Widget>[
-                TextFormField(
-                  textAlign: TextAlign.center,
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(
-                    labelText: 'Comprimento',
-                  ),
-                  inputFormatters: <TextInputFormatter>[
-                    FilteringTextInputFormatter.allow(
-                      RegExp(r'[0-9!.]'),
+        child: SingleChildScrollView(
+          child: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20), color: Colors.white60),
+            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 40),
+            margin: const EdgeInsets.symmetric(horizontal: 0, vertical: 16),
+            child: Align(
+              alignment: Alignment.center,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  TextFormField(
+                    textAlign: TextAlign.center,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                      labelText: 'Comprimento',
                     ),
-                  ],
-                  onChanged: (value) {
-                    if (value != '') {
-                      setState(() {
-                        _cubic.length = double.parse(value);
-                      });
-                    } else {
-                      _cubic.length = 0;
-                    }
-                  },
-                ),
-                TextFormField(
-                  textAlign: TextAlign.center,
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(
-                    labelText: 'Largura',
-                  ),
-                  inputFormatters: <TextInputFormatter>[
-                    FilteringTextInputFormatter.allow(
-                      RegExp(r'[0-9!.]'),
-                    ),
-                  ],
-                  onChanged: (value) {
-                    if (value != '') {
-                      setState(() {
-                        _cubic.size = double.parse(value);
-                      });
-                    } else {
-                      _cubic.length = 0;
-                    }
-                  },
-                ),
-                TextFormField(
-                  textAlign: TextAlign.center,
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(
-                    labelText: 'Altura',
-                  ),
-                  inputFormatters: <TextInputFormatter>[
-                    FilteringTextInputFormatter.allow(
-                      RegExp(r'[0-9!.]'),
-                    ),
-                  ],
-                  onChanged: (value) {
-                    if (value != '') {
-                      setState(() {
-                        _cubic.heigth = double.parse(value);
-                      });
-                    } else {
-                      _cubic.length = 0;
-                    }
-                  },
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: OutlinedButton(
-                    style: const ButtonStyle(
-                      // alignment: AlignmentDirectional.bottomCenter,
-                      elevation: MaterialStatePropertyAll(5),
-                      padding: MaterialStatePropertyAll<EdgeInsets>(
-                          EdgeInsets.only(
-                              top: 20, bottom: 20, left: 80, right: 80)),
-                      backgroundColor: MaterialStatePropertyAll<Color>(
-                        Color.fromARGB(255, 206, 228, 180),
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.allow(
+                        RegExp(r'[0-9!.]'),
                       ),
+                    ],
+                    onChanged: (value) {
+                      if (value != '') {
+                        setState(() {
+                          _cubic.length = double.parse(value);
+                        });
+                      } else {
+                        _cubic.length = 0;
+                      }
+                    },
+                  ),
+                  TextFormField(
+                    textAlign: TextAlign.center,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                      labelText: 'Largura',
                     ),
-                    onPressed: () => {
-                      if (!_cubic.isEmpty())
-                        {
-                          setState(() {
-                            if (kDebugMode) {
-                              print(_cubic.getString());
-                            }
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.allow(
+                        RegExp(r'[0-9!.]'),
+                      ),
+                    ],
+                    onChanged: (value) {
+                      if (value != '') {
+                        setState(() {
+                          _cubic.size = double.parse(value);
+                        });
+                      } else {
+                        _cubic.length = 0;
+                      }
+                    },
+                  ),
+                  TextFormField(
+                    textAlign: TextAlign.center,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                      labelText: 'Altura',
+                    ),
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.allow(
+                        RegExp(r'[0-9!.]'),
+                      ),
+                    ],
+                    onChanged: (value) {
+                      if (value != '') {
+                        setState(() {
+                          _cubic.heigth = double.parse(value);
+                        });
+                      } else {
+                        _cubic.length = 0;
+                      }
+                    },
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: OutlinedButton(
+                      style: const ButtonStyle(
+                        // alignment: AlignmentDirectional.bottomCenter,
+                        elevation: MaterialStatePropertyAll(5),
+                        padding: MaterialStatePropertyAll<EdgeInsets>(
+                            EdgeInsets.only(
+                                top: 20, bottom: 20, left: 80, right: 80)),
+                        backgroundColor: MaterialStatePropertyAll<Color>(
+                          Color.fromARGB(255, 206, 228, 180),
+                        ),
+                      ),
+                      onPressed: () => {
+                        if (!_cubic.isEmpty())
+                          {
+                            setState(() {
+                              if (kDebugMode) {
+                                print(_cubic.getString());
+                              }
+                              showDialog(
+                                context: context,
+                                builder: (context) =>
+                                    HomeWidgets().dialogPopUp(context, _cubic),
+                              );
+                            }),
+                          }
+                        else
+                          {
                             showDialog(
                               context: context,
                               builder: (context) =>
-                                  HomeWidgets().dialogPopUp(context, _cubic),
-                            );
-                          }),
-                        }
-                      else
-                        {
-                          showDialog(
-                            context: context,
-                            builder: (context) =>
-                                HomeWidgets().msgPopUp('Campo(s) vazio(s)'),
-                          )
-                        },
-                    },
-                    child: const Text(
-                      'CALCULAR',
-                      style: TextStyle(color: Colors.black, fontSize: 20),
+                                  HomeWidgets().msgPopUp('Campo(s) vazio(s)'),
+                            )
+                          },
+                      },
+                      child: const Text(
+                        'CALCULAR',
+                        style: TextStyle(color: Colors.black, fontSize: 20),
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
--- lib/features/log/presentation/pages/login_page.dart
+++ lib/features/log/presentation/pages/login_page.dart
@@ -54,92 +54,96 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       extendBodyBehindAppBar: true,
       body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage(globals.backgroundImage), fit: BoxFit.cover),
-        ),
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              flex: 4,
-              child: Align(
-                alignment: Alignment.center,
-                child: Image.asset(globals.logoImage),
-              ),
-            ),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: TextButton.icon(
-                onPressed: () {
-                  setState(() {
-                    isLogin = !isLogin;
-                  });
-                },
-                label: Text(
-                  isLogin ? 'Registrar' : 'Entrar',
-                  style: const TextStyle(color: Colors.blueGrey),
+          padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 20),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage(globals.backgroundImage), fit: BoxFit.cover),
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.center,
+                  child: Image.asset(globals.logoImage),
                 ),
-                icon: Icon(isLogin ? Icons.arrow_forward : Icons.arrow_back,
-                    color: Colors.blueGrey),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(40),
-              margin: const EdgeInsets.all(10),
-              decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
-                  color: Colors.transparent),
-              child: Column(
-                children: <Widget>[
-                  formTextField('Email'),
-                  formTextField('Senha'),
-                  !isLogin ? formTextField('Repita a senha') : const SizedBox(),
-                  const SizedBox(height: 50),
-                  OutlinedButton(
-                    style: const ButtonStyle(
-                      elevation: MaterialStatePropertyAll(5),
-                      padding: MaterialStatePropertyAll<EdgeInsets>(
-                        EdgeInsets.only(
-                            top: 20, bottom: 20, left: 80, right: 80),
-                      ),
-                      backgroundColor: MaterialStatePropertyAll<Color>(
-                        Color.fromARGB(255, 206, 228, 180),
-                      ),
-                    ),
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: TextButton.icon(
                     onPressed: () {
-                      if (isLogin) {
-                        UserController()
-                            .signInWithEmailAndPassword(_email, _password);
-                      } else {
-                        UserController().createUserWithEmailAndPassword(
-                            _email, _password, _passwordValidate);
-                      }
+                      setState(() {
+                        isLogin = !isLogin;
+                      });
                     },
-                    child: Text(
-                      isLogin ? 'ENTRAR' : 'CADASTRAR',
-                      style: const TextStyle(
-                          color: Colors.black,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
+                    label: Text(
+                      isLogin ? 'Registrar' : 'Entrar',
+                      style: const TextStyle(color: Colors.blueGrey),
                     ),
+                    icon: Icon(isLogin ? Icons.arrow_forward : Icons.arrow_back,
+                        color: Colors.blueGrey),
                   ),
-                  Text(
-                    '${error.value}',
-                    style: const TextStyle(color: Colors.redAccent),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(40),
+                  margin: const EdgeInsets.all(10),
+                  decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      color: Colors.transparent),
+                  child: Column(
+                    children: <Widget>[
+                      formTextField('Email'),
+                      formTextField('Senha'),
+                      !isLogin
+                          ? formTextField('Repita a senha')
+                          : const SizedBox(),
+                      const SizedBox(height: 50),
+                      OutlinedButton(
+                        style: const ButtonStyle(
+                          elevation: MaterialStatePropertyAll(5),
+                          padding: MaterialStatePropertyAll<EdgeInsets>(
+                            EdgeInsets.symmetric(horizontal: 32, vertical: 8),
+                          ),
+                          backgroundColor: MaterialStatePropertyAll<Color>(
+                            Color.fromARGB(255, 206, 228, 180),
+                          ),
+                        ),
+                        onPressed: () {
+                          if (isLogin) {
+                            UserController()
+                                .signInWithEmailAndPassword(_email, _password);
+                          } else {
+                            UserController().createUserWithEmailAndPassword(
+                                _email, _password, _passwordValidate);
+                          }
+                        },
+                        child: Text(
+                          isLogin ? 'ENTRAR' : 'CADASTRAR',
+                          style: const TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Text(
+                        '${error.value}',
+                        style: const TextStyle(color: Colors.redAccent),
+                      ),
+                      TextButton(
+                        onPressed: () => {context.push('/recovery')},
+                        child: const Text(
+                          'Recuperar cadastro',
+                          style:
+                              TextStyle(color: Colors.blueGrey, fontSize: 15),
+                        ),
+                      ),
+                    ],
                   ),
-                  TextButton(
-                    onPressed: () => {context.push('/recovery')},
-                    child: const Text(
-                      'Recuperar cadastro',
-                      style: TextStyle(color: Colors.blueGrey, fontSize: 15),
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+          )),
     );
   }
 }
